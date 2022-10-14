99b3f7894d99e46c4a3d8baa7cc0c3aaeb1c05a6
imp <106537315+imptype@users.noreply.github.com>
2022-10-14 17:04:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from deta_discord_interactions import DiscordInteractions  
+#from deta_discord_interactions import DiscordInteractions  
 
 app = Flask(__name__)  # notice that the app instance is called `app`, this is very important.
-discord = DiscordInteractions(app)
+#discord = DiscordInteractions(app)
 
 app.config["DISCORD_CLIENT_ID"] = os.environ["DISCORD_CLIENT_ID"]
 app.config["DISCORD_PUBLIC_KEY"] = os.environ["DISCORD_PUBLIC_KEY"]
@@ -12,8 +12,8 @@ app.config["DISCORD_CLIENT_SECRET"] = os.environ["DISCORD_CLIENT_SECRET"]
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@discord.command()
-def ping(ctx):
-    "Respond with a friendly 'pong'!"
-    return "Pong!"
+#@discord.command()
+#def ping(ctx):
+#    "Respond with a friendly 'pong'!"
+#    return "Pong!"
 
