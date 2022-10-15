51a43b171f32333feda415c7f894e7589e704040
imp <106537315+imptype@users.noreply.github.com>
2022-10-15 12:48:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def ping(ctx):
 
 @app.route('/')
 def index(request, start_response, abort):
-    return 'Home'
+    start_response('200 OK', [])
+    return ['Home'.encode('UTF-8')]
 
 @app.route('/update_commands')
 def home(request, start_response, abort):
-    #app.update_commands(from_inside_a_micro=True)
+    app.update_commands(from_inside_a_micro=True)
     start_response('200 OK', [])
     return ['updated commands'.encode('UTF-8')]
