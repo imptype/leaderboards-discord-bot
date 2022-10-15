b50495aa45d1f2d10f215bbb92b7b8eb532ad39c
imp <106537315+imptype@users.noreply.github.com>
2022-10-15 12:43:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ app = DiscordInteractions()
 
 @app.command()
 def ping(ctx):
-    "Respond with a friendly 'pong'!"
-    return "Pong!"
+    return 'Pong!'
+
+@app.route('/')
+def index(request, start_response, abort):
+    return 'Home'
 
 @app.route('/update_commands')
 def home(request, start_response, abort):
-    app.update_commands(from_inside_a_micro=True)
+    #app.update_commands(from_inside_a_micro=True)
     start_response('200 OK', [])
     return ['updated commands'.encode('UTF-8')]
