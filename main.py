dbd0690de8d85fbbbdebdc0db45398517062d6a2
imp <106537315+imptype@users.noreply.github.com>
2022-10-15 13:17:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from deta_discord_interactions import DiscordInteractions  
+import traceback
 
 app = DiscordInteractions()
 
@@ -19,6 +20,6 @@ def home(request, start_response, abort):
         app.update_commands(from_inside_a_micro=True)
     except Exception as e:
         func = app.update_commands
-        text = '\n'.join([str(e), str(func), str(func.__code__.co_argcount), str(func.__code__.co_varnames)])
+        text = '\n'.join([str(e), str(func), str(func.__code__.co_argcount), str(func.__code__.co_varnames), traceback.format_exc()])
     start_response('200 OK', [])
     return [text.encode('UTF-8')]
